b4df78303b0dd2243485c49664935a70fccdd701
Naman-NITA <namank89123@gmail.com>
2025-06-16 22:20:11 +0530

    Fix import

--- src/components/Connections.jsx
+++ src/components/Connections.jsx
@@ -5,6 +5,8 @@ import { useDispatch, useSelector } from "react-redux";
 import { addConnections } from "../utils/conectionSlice";
 import { Link } from "react-router-dom";
 
+//make ir
+
 const Connections = () => {
   const connections = useSelector((store) => store.connections);
   const dispatch = useDispatch();
