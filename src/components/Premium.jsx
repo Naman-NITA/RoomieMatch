2ae3166fc539c5dc50eaedfe35ef07873ff2dc71
Naman-NITA <namank89123@gmail.com>
2025-07-06 23:19:52 +0530

    Fix import issues for Vercel

--- src/components/Premium.jsx
+++ src/components/Premium.jsx
@@ -1,13 +1,27 @@
-import React from 'react';
-
+import axios from "axios";
 import { BASE_URL } from "../utils/constants";
-import axios from 'axios';
+import { useEffect, useState } from "react";
 
 
 const Premium = () => {
+  const [isUserPremium, setIsUserPremium] = useState(false);
+  useEffect(() => {
+    verifyPremiumUser();
+  }, []);
+
+  const verifyPremiumUser = async () => {
+    const res = await axios.get(BASE_URL + "/premium/verify", {
+      withCredentials: true,
+    });
+
+    if (res.data.isPremium) {
+      setIsUserPremium(true);
+    }
+  };
+
   const handleBuyClick = async (type) => {
     const order = await axios.post(
-      BASE_URL  + "/payment/create",
+      BASE_URL + "/payment/create",
       {
         membershipType: type,
       },
@@ -17,7 +31,7 @@ const Premium = () => {
     const { amount, keyId, currency, notes, orderId } = order.data;
 
     const options = {
-      key:keyId,
+      key: keyId,
       amount,
       currency,
       name: "Dev Tinder",
@@ -31,44 +45,43 @@ const Premium = () => {
       theme: {
         color: "#F37254",
       },
-     
-      
+      handler: verifyPremiumUser,
     };
 
     const rzp = new window.Razorpay(options);
     rzp.open();
   };
-  return (
+  return isUserPremium ? (
+    "You're are already a premium user"
+  ) : (
     <div className="m-10">
       <div className="flex w-full">
-        <div className="card bg-base-300 rounded-box grid h-80 flex-grow place-items-center p-4">
+        <div className="card bg-base-300 rounded-box grid h-80 flex-grow place-items-center">
           <h1 className="font-bold text-3xl">Silver Membership</h1>
-          <ul className="mt-2 mb-4 list-disc list-inside">
-            <li>Chat with other people</li>
-            <li>100 connection requests per day</li>
-            <li>Blue Tick</li>
-            <li>3 months</li>
+          <ul>
+            <li> - Chat with other people</li>
+            <li> - 100 connection Requests per day</li>
+            <li> - Blue Tick</li>
+            <li> - 3 months</li>
           </ul>
           <button
-            onClick={() => handleBuyClick('silver')}
+            onClick={() => handleBuyClick("gold")}
             className="btn btn-secondary"
           >
             Buy Silver
           </button>
         </div>
-
         <div className="divider divider-horizontal">OR</div>
-
-        <div className="card bg-base-300 rounded-box grid h-80 flex-grow place-items-center p-4">
+        <div className="card bg-base-300 rounded-box grid h-80 flex-grow place-items-center">
           <h1 className="font-bold text-3xl">Gold Membership</h1>
-          <ul className="mt-2 mb-4 list-disc list-inside">
-            <li>Chat with other people</li>
-            <li>Infinite connection requests per day</li>
-            <li>Blue Tick</li>
-            <li>6 months</li>
+          <ul>
+            <li> - Chat with other people</li>
+            <li> - Inifiniye connection Requests per day</li>
+            <li> - Blue Tick</li>
+            <li> - 6 months</li>
           </ul>
           <button
-            onClick={() => handleBuyClick('gold')}
+            onClick={() => handleBuyClick("gold")}
             className="btn btn-primary"
           >
             Buy Gold
@@ -78,5 +91,4 @@ const Premium = () => {
     </div>
   );
 };
-
 export default Premium;
